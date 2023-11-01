9b68bf2bff72c3e363e7bc8d081c83b434acf557
tru69er <chidori9912@gmail.com>
2023-11-01 16:31:09 +0530

    edit contact

--- db/methods.go
+++ db/methods.go
@@ -8,28 +8,7 @@ import (
 	"log"
 	"net/http"
 	"text/template"
-
-	"github.com/fjl/go-couchdb"
 )
-
-func ConnectDB(url string, l *log.Logger) (*couchdb.DB, error) {
-	client, err := couchdb.NewClient(url, http.DefaultTransport)
-
-	if err != nil {
-		l.Println("could not connect to db")
-		return nil, err
-	}
-
-	l.Println("Connected to db server")
-
-	db := client.DB("sdb_test")
-
-	if db == nil {
-		return nil, errors.New("DB not found")
-	}
-
-	return db, nil
-}
 
 func FindOne(l *log.Logger, email string) []byte {
 
--- handlers/contact.go
+++ handlers/contact.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"io"
 	"log"
 	"net/http"
 
@@ -26,12 +27,36 @@ func (c Contact) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			c.findOne(w, email)
 		}
 	case http.MethodPost:
-		c.addOne(w, r)
+		c.addOne(w, r.Body)
 	case http.MethodDelete:
 		c.deleteOne(w, email)
+	case http.MethodPut:
+		c.updateOne(w, r.Body, email)
 	default:
 		http.Error(w, "ki korchish bhai... keno korchish erom", http.StatusMethodNotAllowed)
 	}
+}
+
+func (c Contact) updateOne(w http.ResponseWriter, body io.Reader, email string) {
+	contact := contact.Contact{}
+
+	contact.FromJSON(body)
+
+	cont, err := db.ToDoc(db.FindOne(c.l, email))
+
+	if err != nil {
+		c.l.Println(err)
+		http.Error(w, "Error in fetching document from db", http.StatusInternalServerError)
+	}
+
+	_, err = c.db.Put(cont.Docs[0].Id, contact, cont.Docs[0].Rev)
+
+	if err != nil {
+		c.l.Println(err)
+		http.Error(w, "Failed to update document in db", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(200)
 }
 
 func (c Contact) deleteOne(w http.ResponseWriter, email string) {
@@ -56,9 +81,9 @@ func (c Contact) deleteOne(w http.ResponseWriter, email string) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func (c Contact) addOne(w http.ResponseWriter, r *http.Request) {
+func (c Contact) addOne(w http.ResponseWriter, body io.Reader) {
 	new := contact.Contact{}
-	new.FromJSON(r.Body)
+	new.FromJSON(body)
 	c.l.Println(new)
 	_, err := c.db.Put(uuid.NewString(), new, "")
 
